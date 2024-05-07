281fe5bbcc64f0266c6580c414f07698e0f06ba3
GajendraRao007 <raogajendra27@gmail.com>
2024-05-07 13:58:41 +0530

    added popup on click of addBtn

--- src/Components/Notesapp.jsx
+++ src/Components/Notesapp.jsx
@@ -1,12 +1,24 @@
 import addNotesButton from '../Images/button.png';
 import backgroundimg from '../Images/bg.png';
 import lockimg from '../Images/lock.png';
+import { useState } from 'react';
 
 function Notesapp() {
+
+    const [popup, setPopup] = useState(false)
+    const [bgOverlay, setBgOverlay] = useState(false);
+
+   
+
+    function addButton (){
+        setPopup(true)
+        setBgOverlay(true); 
+    }
+
     return(
         <>
-        <div className="container">
-             <div className="side-bar">
+            <div className="container">
+            <div className="side-bar">
                 <h1 className="sidebar-text">Pocket Notes</h1>
 
             <div class="scroll">
@@ -65,7 +77,7 @@ function Notesapp() {
             </div>
 
             </div>   
-                <img className="addnotes-btn" src={addNotesButton} alt="" srcset="" />
+                <img className="addnotes-btn" onClick={addButton} src={addNotesButton} alt="" srcset="" />
 
         <div className="notes-section">
             <img className='bg-img' src={backgroundimg} alt="" />
@@ -79,7 +91,30 @@ function Notesapp() {
             </div>
 
             </div>
+
+
         </div>
+
+    {popup && (
+      <div className='popup-bg'>
+      <div className='pop-up'>
+      <h1 className='popup-title'>Create New group</h1>
+      <label>Group Name</label><input className='input-groupname' type="text" placeholder='Enter group name' />
+      <div className='color-box'>
+      <h1 className='color-title'>Choose colour</h1>
+      <button className='purple'></button>
+      <button className='pink'></button>
+      <button className='cyan'></button>
+      <button className='orange'></button>
+      <button className='darkblue'></button>
+      <button className='lightblue'></button>
+      </div>
+      <button className='create-btn'>Create</button>
+      </div> 
+  </div>
+  )}
+
+{bgOverlay && <div className="background-overlay"></div>}
         </>
     )
 }
