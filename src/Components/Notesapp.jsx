d9bb517fed52814a0920cfa040aa0cdc279355e0
GajendraRao007 <raogajendra27@gmail.com>
2024-05-08 16:03:45 +0530

    added functionality - every group had its own notes-section to add the Notes seperately

--- src/Components/Notesapp.jsx
+++ src/Components/Notesapp.jsx
@@ -14,6 +14,8 @@ function Notesapp() {
     const [groups, setGroups] = useState([]);
     const [groupContent, setGroupContent] = useState({});
     const [noteText, setNoteText] = useState("");
+    const [selectedGroup, setSelectedGroup] = useState(null);
+    const [isGroupClicked, setIsGroupClicked] = useState(false);
 
     const groupTitle = (e) => {
         setTitle(e.target.value);
@@ -32,7 +34,7 @@ function Notesapp() {
         if (title.trim() !== "") {
             const newGroup = { title: title, color: selectedColor };
             setGroups([...groups, newGroup]);
-            setGroupContent({ ...groupContent, [title]: "" });
+            setGroupContent({ ...groupContent, [title]: [] });
             setTitle("");
             setPopup(false);
             setBgOverlay(false);
@@ -43,10 +45,15 @@ function Notesapp() {
         if (noteText.trim() !== "") {
             setGroupContent({
                 ...groupContent,
-                [title]: [...(groupContent[title] || []), noteText]
+                [selectedGroup]: [...(groupContent[selectedGroup] || []), noteText]
             });
             setNoteText("");
         }
+    }
+
+    function handleGroupClick(groupTitle) {
+        setSelectedGroup(groupTitle);
+        setIsGroupClicked(true);
     }
 
     return (
@@ -66,67 +73,69 @@ function Notesapp() {
                 </div>
                 <img className="addnotes-btn" onClick={addButton} src={addNotesButton} alt="" srcSet="" />
 
-                {/* <div className="home-section">
-                    <img className='bg-img' src={backgroundimg} alt="" />
-                    <h1 className='notes-section-text'>Pocket Notes</h1>
-                    <p className='notes-text-description'>Send and receive messages without keeping your phone online. <br />
-                        Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
-                    <div className='footer-box'>
-                        <img src={lockimg} alt="" />
-                        <p className='footer-text'> end-to-end encrypted</p>
-                    </div>                    
-                </div> */}
-
-                <div className='notes-section'>
-                    <div className='header'>
-                        <h1 className='header-circle'>MN</h1>
-                        <h1 className='notes-title'>My Notes</h1>
-                    </div>
-
-                    <div className='notesbox'>
-                        {groupContent[title] && groupContent[title].map((note, index) => (
-                            <div key={index} className='notes'>
-                                <p className='txt'>{note}</p>
-                                <div className='date-time'>
-                                    <p>9 Mar 2023</p>
-                                    <div>
-                                        <img src={bulletimg} alt="" />
-                                    </div>
-                                    <p>10:10 AM</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-
-                    <div className='notes-inputbox'>
-                        <textarea className='input-notes' value={noteText} onChange={(e) => setNoteText(e.target.value)} placeholder='Enter your text here...........' />
-                        <img className='addbtn' onClick={addNote} src={sendimg} alt="" srcset="" />
-                    </div>
-
-                </div>
-            </div>
-
-            {popup && (
-                <div className='popup-bg'>
-                    <div className='pop-up'>
-                        <h1 className='popup-title'>Create New group</h1>
-                        <label>Group Name</label>
-                        <input className='input-groupname' onChange={groupTitle} type="text" placeholder='Enter group name' />
-                        <div className='color-box'>
-                            <h1 className='color-title'>Choose colour</h1>
-                            <button className='purple' onClick={() => handleColorChange("#B38BFA")}></button>
-                            <button className='pink' onClick={() => handleColorChange("#FF79F2")}></button>
-                            <button className='cyan' onClick={() => handleColorChange("#43E6FC")}></button>
-                            <button className='orange' onClick={() => handleColorChange("#F19576")}></button>
-                            <button className='darkblue' onClick={() => handleColorChange("#0047FF")}></button>
-                            <button className='lightblue' onClick={() => handleColorChange("#6691FF")}></button>
+                {isGroupClicked ? null : (
+                    <div className="home-section">
+                        <img className='bg-img' src={backgroundimg} alt="" />
+                        <h1 className='notes-section-text'>Pocket Notes</h1>
+                        <p className='notes-text-description'>Send and receive messages without keeping your phone online. <br />
+                            Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+                        <div className='footer-box'>
+                            <img src={lockimg} alt="" />
+                            <p className='footer-text'> end-to-end encrypted</p>
                         </div>
-                        <button className='create-btn' onClick={createGroup}>Create</button>
                     </div>
-                </div>
-            )}
+                )}
+                {groups.map((group, index) => (
+                    <div key={index} className='notes-section' style={{ display: selectedGroup === group.title ? 'block' : 'none' }}>
+                        <div className='header'>
+                            <h1 className='header-circle'>{group.title.split(' ').slice(0, 2).map(word => word.charAt(0)).join('').toUpperCase()}</h1>
+                            <h1 className='notes-title'>{group.title}</h1>
+                        </div>
 
-            {bgOverlay && <div className="background-overlay"></div>}
+                        <div className='notesbox'>
+                            {groupContent[group.title] && groupContent[group.title].map((note, index) => (
+                                <div key={index} className='notes'>
+                                    <p className='txt'>{note}</p>
+                                    <div className='date-time'>
+                                        <p>9 Mar 2023</p>
+                                        <div>
+                                            <img src={bulletimg} alt="" />
+                                        </div>
+                                        <p>10:10 AM</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className='notes-inputbox'>
+                            <textarea className='input-notes' value={noteText} onChange={(e) => setNoteText(e.target.value)} placeholder='Enter your text here...........' />
+                            <img className='addbtn' onClick={addNote} src={sendimg} alt="" srcSet="" />
+                        </div>
+                    </div>
+                ))}
+
+                {popup && (
+                    <div className='popup-bg'>
+                        <div className='pop-up'>
+                            <h1 className='popup-title'>Create New group</h1>
+                            <label>Group Name</label>
+                            <input className='input-groupname' onChange={groupTitle} type="text" placeholder='Enter group name' />
+                            <div className='color-box'>
+                                <h1 className='color-title'>Choose colour</h1>
+                                <button className='purple' onClick={() => handleColorChange("#B38BFA")}></button>
+                                <button className='pink' onClick={() => handleColorChange("#FF79F2")}></button>
+                                <button className='cyan' onClick={() => handleColorChange("#43E6FC")}></button>
+                                <button className='orange' onClick={() => handleColorChange("#F19576")}></button>
+                                <button className='darkblue' onClick={() => handleColorChange("#0047FF")}></button>
+                                <button className='lightblue' onClick={() => handleColorChange("#6691FF")}></button>
+                            </div>
+                            <button className='create-btn' onClick={createGroup}>Create</button>
+                        </div>
+                    </div>
+                )}
+
+                {bgOverlay && <div className="background-overlay"></div>}
+            </div>
         </>
     )
 }
